8eb129447a92adaa9af9b95732158b37bd0a1ae6
carlopezc <carlotalcd@gmail.com>
2025-06-01 01:51:34 +0200

    soy lucas

--- src/ft_parsing.c
+++ src/ft_parsing.c
@@ -6,7 +6,7 @@
 /*   By: carlotalcd <carlotalcd@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 12:44:53 by carlopez          #+#    #+#             */
-/*   Updated: 2025/05/30 19:31:36 by carlopez         ###   ########.fr       */
+/*   Updated: 2025/06/01 01:49:51 by carlopez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,14 +89,87 @@ void	ft_last(t_token **token)
 	return ;
 }
 
+int	ft_add_bracket_token2(t_token **tmp, t_token *prev, int *i, t_token **token)
+{
+	char	*str;
+	int		start;
+	t_token	*new_token;
+
+	str = (*tmp)->str;
+	start = 0;
+	if (str[*i] == '(' && (!str[*i - 1] || str[*i - 1] != '\\'))
+	{
+		start = *i;
+		new_token = ft_create_token("(", O_BRACKET);
+		if (!new_token)
+			return (0);
+		ft_connect_token(token, new_token, prev);
+		(*tmp)->str = ft_substr(str, start + 1,
+				ft_strlen(str) - (start + 1));
+		str = (*tmp)->str;
+		(*i)--;
+		return (2);
+	}
+	return (1);
+}
+
+int	ft_add_bracket_token3(t_token **tmp, int *i, t_token **token)
+{
+	char	*str;
+	t_token	*new_token;
+	char	*cpy;
+
+	str = (*tmp)->str;
+	cpy = NULL;
+	if (str[*i] == ')' && (!str[(*i) - 1] || str[(*i) - 1] != '\\'))
+	{
+		cpy = ft_strdup(str);
+		(*tmp)->str = ft_substr(str, 0, *i);
+		while (tmp && cpy[*i]
+			&& cpy[*i] == ')' && (!cpy[(*i) - 1] || cpy[(*i)] != '\\'))
+		{
+			new_token = ft_create_token(")", C_BRACKET);
+			if (!new_token)
+				return (0);
+			ft_connect_token(token, new_token, *tmp);
+			(*tmp) = (*tmp)->next;
+			(*i)++;
+		}
+		if (!cpy[*i])
+			return (2);
+	}
+	return (1);
+}
+
+int	ft_add_brackets_loop(t_token *prev, t_token **token, t_token **tmp)
+{
+	int		i;
+	int		res;
+	char	*str;
+
+	i = 0;
+	str = (*tmp)->str;
+	while (str[i])
+	{
+		res = ft_add_bracket_token2(tmp, prev, &i, token);
+		if (!res)
+			return (0);
+		if (res == 1)
+		{
+			res = ft_add_bracket_token3(tmp, &i, token);
+			if (res == 0)
+				return (0);
+			if (res == 2)
+				break ;
+		}
+		i++;
+	}
+	return (1);
+}
+
 int	ft_add_bracket_token(t_token **token)
 {
 	t_token	*tmp;
-	char	*str;
-	int		i;
-	int		start;
-	char	*cpy;
-	t_token	*new_token;
 	t_token	*prev;
 
 	if (!ft_manage_brackets(*token))
@@ -104,51 +177,13 @@ int	ft_add_bracket_token(t_token **token)
 	tmp = *token;
 	if (!tmp)
 		return (1);
-	str = tmp->str;
-	i = 0;
-	start = 0;
-	cpy = NULL;
 	prev = NULL;
 	while (tmp)
 	{
-		str = tmp->str;
-		while (str[i])
-		{
-			if (str[i] == '(' && (!str[i - 1] || str[i - 1] != '\\'))
-			{
-				start = i;
-				new_token = ft_create_token("(", O_BRACKET);
-				if (!new_token)
-					return (0);
-				ft_connect_token(token, new_token, prev);
-				tmp->str = ft_substr(str, start + 1,
-						ft_strlen(str) - (start + 1));
-				str = tmp->str;
-				i--;
-			}
-			else if (str[i] == ')' && (!str[i - 1] || str[i - 1] != '\\'))
-			{
-				cpy = ft_strdup(str);
-				tmp->str = ft_substr(str, 0, i);
-				while (tmp && cpy[i]
-					&& cpy[i] == ')' && (!cpy[i - 1] || cpy[i] != '\\'))
-				{
-					new_token = ft_create_token(")", C_BRACKET);
-					if (!new_token)
-						return (0);
-					ft_connect_token(token, new_token, tmp);
-					tmp = tmp->next;
-					i++;
-				}
-				if (!cpy[i])
-					break ;
-			}
-			i++;
-		}
+		if (!ft_add_brackets_loop(prev, token, &tmp))
+			return (0);
 		prev = tmp;
 		tmp = tmp->next;
-		start = 0;
-		i = 0;
 	}
 	ft_last(token);
 	return (1);

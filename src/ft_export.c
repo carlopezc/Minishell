e73b518d1cc8a92e835524c77c43126188f994db
carlopezc <carlotalcd@gmail.com>
2025-07-26 05:10:25 +0200

    exit status export

--- src/ft_export.c
+++ src/ft_export.c
@@ -6,7 +6,7 @@
 /*   By: carlotalcd <carlotalcd@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 16:13:37 by carlopez          #+#    #+#             */
-/*   Updated: 2025/07/26 04:05:53 by carlopez         ###   ########.fr       */
+/*   Updated: 2025/07/26 05:09:50 by carlopez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,16 +51,12 @@ static int	ft_export_loop(char **split, t_minishell *shell)
 	t_env	*node;
 	char	*name;
 
-	//wexit status 1??????
 	i = 1;
 	name = NULL;
 	while (split[i])
 	{
 		if (!ft_check_name(split[i]))
-		{
-			//aqui si da error non valid identifier == 1 (creo) y acaba
-			return (0);
-		}
+			return (ft_finish_build(1, shell), 0);
 		if (!ft_export_aux(split, i, shell, &node))
 		{
 			if (!ft_check_duplicated(split[i],
@@ -73,7 +69,6 @@ static int	ft_export_loop(char **split, t_minishell *shell)
 		}
 		i++;
 	}
-	//wexit status 0 si todo bien
 	return (1);
 }
 
